ce6776842225d9d6bd23af66e94a367aac5df280
Akshay200228 <sankpla2812@gmail.com>
2023-06-20 23:27:13 +0530

    add cards update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,7 +8,9 @@ import {
     CardFooter,
 } from '@material-tailwind/react';
 import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+import { Carousel } from 'react-responsive-carousel';
 import { card1, card2, card3 } from '../assets';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Cards = () => {
     return (
@@ -44,7 +46,9 @@ const Cards = () => {
                     </a>
                 </CardBody>
             </Card>
-            <div className="flex mt-12">
+
+            {/* 2nd and 3rd Cards - Only in Desktop View */}
+            <div className="hidden mt-12 md:flex">
                 {/* 2nd Card */}
                 <Card className="w-1/2 mr-2 bg-gray-900">
                     <CardHeader color="blue-gray" className="relative bg-gray-900 h-80">
@@ -56,7 +60,7 @@ const Cards = () => {
                         </Typography>
                         <Typography>
                             The place is close to Barceloneta Beach and bus stop just 2 min by walk
-                            and near to &quot;Naviglio&quot; where you can enjoy the main night life
+                            and near to "Naviglio" where you can enjoy the main night life
                             in Barcelona.
                         </Typography>
                     </CardBody>
@@ -76,7 +80,7 @@ const Cards = () => {
                         </Typography>
                         <Typography>
                             The place is close to Barceloneta Beach and bus stop just 2 min by walk
-                            and near to &quot;Naviglio&quot; where you can enjoy the main night life
+                            and near to "Naviglio" where you can enjoy the main night life
                             in Barcelona.
                         </Typography>
                     </CardBody>
@@ -84,6 +88,52 @@ const Cards = () => {
                         <Button>Read More</Button>
                     </CardFooter>
                 </Card>
+            </div>
+
+            {/* Carousel - Only in Mobile View */}
+            <div className="mt-12 md:hidden">
+                <Carousel showStatus={false} showThumbs={false}>
+                    <div>
+                        <Card className="w-full bg-gray-900">
+                            <CardHeader color="blue-gray" className="relative bg-gray-900 h-80">
+                                <img src={card2} alt="img-blur-shadow" className="w-full h-full" />
+                            </CardHeader>
+                            <CardBody>
+                                <Typography variant="h5" color="blue-gray" className="mb-2">
+                                    UI/UX Review Check
+                                </Typography>
+                                <Typography>
+                                    The place is close to Barceloneta Beach and bus stop just 2 min by walk
+                                    and near to "Naviglio" where you can enjoy the main night life
+                                    in Barcelona.
+                                </Typography>
+                            </CardBody>
+                            <CardFooter className="pt-0">
+                                <Button>Read More</Button>
+                            </CardFooter>
+                        </Card>
+                    </div>
+                    <div>
+                        <Card className="w-full bg-gray-900">
+                            <CardHeader color="blue-gray" className="relative bg-gray-900 h-80">
+                                <img src={card3} alt="img-blur-shadow" className="w-full h-full" />
+                            </CardHeader>
+                            <CardBody>
+                                <Typography variant="h5" color="blue-gray" className="mb-2">
+                                    UI/UX Review Check
+                                </Typography>
+                                <Typography>
+                                    The place is close to Barceloneta Beach and bus stop just 2 min by walk
+                                    and near to "Naviglio" where you can enjoy the main night life
+                                    in Barcelona.
+                                </Typography>
+                            </CardBody>
+                            <CardFooter className="pt-0">
+                                <Button>Read More</Button>
+                            </CardFooter>
+                        </Card>
+                    </div>
+                </Carousel>
             </div>
         </div>
     );
