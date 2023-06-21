d6b946c0c420efca4a6fe8dd7028337438cd289b
Akshay200228 <sankpla2812@gmail.com>
2023-06-21 13:55:03 +0530

    Start working on payment cards

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,218 @@
-import React from 'react'
+import React from 'react';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from '@material-tailwind/react';
+import { CheckIcon } from '@heroicons/react/24/outline';
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
-  )
-}
+    <section className="grid grid-cols-3 gap-8 mt-20">
+      {/* Card 1 */}
+      <Card color="blue" variant="gradient" className="max-w-[20rem] p-8">
+        {/* Card header */}
+        <CardHeader
+          floated={false}
+          shadow={false}
+          color="transparent"
+          className="pb-8 m-0 mb-8 text-center border-b rounded-none border-white/10"
+        >
+          <Typography variant="small" color="white" className="font-normal uppercase">
+            standard
+          </Typography>
+          <Typography variant="h1" color="white" className="flex justify-center gap-1 mt-6 font-normal text-7xl">
+            <span className="mt-2 text-4xl">$</span>29 <span className="self-end text-4xl">/mo</span>
+          </Typography>
+        </CardHeader>
 
-export default CardDeal
+        {/* Card body */}
+        <CardBody className="p-0">
+          <ul className="flex flex-col gap-4">
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">5 team members</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">200+ components</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">40+ built-in pages</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">1 year free updates</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">Life time technical support</Typography>
+            </li>
+          </ul>
+        </CardBody>
+
+        {/* Card footer */}
+        <CardFooter className="p-0 mt-12">
+          <Button
+            size="lg"
+            color="white"
+            className="text-blue-500 hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
+            ripple={false}
+            fullWidth={true}
+          >
+            Buy Now
+          </Button>
+        </CardFooter>
+      </Card>
+
+      {/* Card 2 */}
+      <Card color="green" variant="gradient" className="max-w-[20rem] p-8">
+        {/* Card header */}
+        <CardHeader
+          floated={false}
+          shadow={false}
+          color="transparent"
+          className="pb-8 m-0 mb-8 text-center border-b rounded-none border-white/10"
+        >
+          <Typography variant="small" color="white" className="font-normal uppercase">
+            premium
+          </Typography>
+          <Typography variant="h1" color="white" className="flex justify-center gap-1 mt-6 font-normal text-7xl">
+            <span className="mt-2 text-4xl">$</span>49 <span className="self-end text-4xl">/mo</span>
+          </Typography>
+        </CardHeader>
+
+        {/* Card body */}
+        <CardBody className="p-0">
+          <ul className="flex flex-col gap-4">
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">10 team members</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">300+ components</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">60+ built-in pages</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">2 years free updates</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">Life time technical support</Typography>
+            </li>
+          </ul>
+        </CardBody>
+
+        {/* Card footer */}
+        <CardFooter className="p-0 mt-12">
+          <Button
+            size="lg"
+            color="white"
+            className="text-green-500 hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
+            ripple={false}
+            fullWidth={true}
+          >
+            Buy Now
+          </Button>
+        </CardFooter>
+      </Card>
+
+      {/* Card 3 (Existing Card) */}
+      <Card color="blue" variant="gradient" className="max-w-[20rem] p-8">
+      <CardHeader
+          floated={false}
+          shadow={false}
+          color="transparent"
+          className="pb-8 m-0 mb-8 text-center border-b rounded-none border-white/10"
+        >
+          <Typography variant="small" color="white" className="font-normal uppercase">
+            premium
+          </Typography>
+          <Typography variant="h1" color="white" className="flex justify-center gap-1 mt-6 font-normal text-7xl">
+            <span className="mt-2 text-4xl">$</span>49 <span className="self-end text-4xl">/mo</span>
+          </Typography>
+        </CardHeader>
+
+        {/* Card body */}
+        <CardBody className="p-0">
+          <ul className="flex flex-col gap-4">
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">10 team members</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">300+ components</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">60+ built-in pages</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">2 years free updates</Typography>
+            </li>
+            <li className="flex items-center gap-4">
+              <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                <CheckIcon strokeWidth={2} className="w-3 h-3" />
+              </span>
+              <Typography className="font-normal">Life time technical support</Typography>
+            </li>
+          </ul>
+        </CardBody>
+
+        {/* Card footer */}
+        <CardFooter className="p-0 mt-12">
+          <Button
+            size="lg"
+            color="white"
+            className="text-green-500 hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
+            ripple={false}
+            fullWidth={true}
+          >
+            Buy Now
+          </Button>
+        </CardFooter>
+      </Card>
+    </section>
+  );
+};
+
+export default CardDeal;
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Card,
   CardBody,
-  CardFooter,
   Typography,
 } from '@material-tailwind/react';
 import styles from '../style';
 import { card } from '../assets';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Features = () => {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section id='features' className='pt-20'>
       <div className={`flex-1 ${styles.flexCenter} flex-col xl:px-0 sm:px-16 px-6`}>
@@ -27,74 +43,126 @@ const Features = () => {
           </h1>
         </div>
 
-        <div className="flex justify-center mt-16">
-          <div className="mx-2">
+        {isMobileView ? (
+          <div className="flex flex-wrap justify-center mt-16">
             {/* Card 1 */}
-            <Card className='bg-slate-900'>
-              <img src={card} alt="Card 1 Header" className="w-full h-auto p-2" />
-              <CardBody>
-                <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
-                  <p className={`${styles.paragraph} ml-2`}>
-                    <span className='text-white'>Fastest</span> Copywriting Your {" "}
-                    <span className='text-white'>Website</span>
-                  </p>
-                </div>
-                <Typography variant="h5" color="blue-gray" className="mb-2 font-bold text-white">
-                  Lyft launching cross-platform
-                </Typography>
-                <p className={`${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at magna vitae lorem dapibus cursus. Nulla viverra diam sed mi ullamcorper aliquet.</p>
-              </CardBody>
-              <CardFooter>
-                {/* Card 1 Footer */}
-              </CardFooter>
-            </Card>
-          </div>
-          <div className="mx-2" style={{ marginTop: '30px' }}>
+            <div className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <Card className='bg-slate-900'>
+                <img src={card} alt="Card 1 Header" className="object-cover w-full h-auto p-2" />
+                <CardBody>
+                  <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+                    <p className={`${styles.paragraph} ml-2`}>
+                      <span className='text-white'>Fastest</span> Copywriting Your {" "}
+                      <span className='text-white'>Website</span>
+                    </p>
+                  </div>
+                  <Typography variant="h5" color="blue-gray" className="mb-2 font-bold text-white">
+                    Lyft launching cross-platform
+                  </Typography>
+                  <p className={`${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at magna vitae lorem dapibus cursus. Nulla viverra diam sed mi ullamcorper aliquet.</p>
+                </CardBody>
+              </Card>
+            </div>
+
             {/* Card 2 */}
-            <Card className='bg-slate-900'>
-              <img src={card} alt="Card 2 Header" className="w-full h-auto p-2" />
-              <CardBody>
-                <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
-                  <p className={`${styles.paragraph} ml-2`}>
-                    <span className='text-white'>Fastest</span> Copywriting Your {" "}
-                    <span className='text-white'>Website</span>
-                  </p>
-                </div>
-                <Typography variant="h5" color="blue-gray" className="mb-2 font-bold text-white">
-                  Lyft launching cross-platform
-                </Typography>
-                <p className={`${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at magna vitae lorem dapibus cursus. Nulla viverra diam sed mi ullamcorper aliquet.</p>
-              </CardBody>
-              <CardFooter>
-                {/* Card 2 Footer */}
-              </CardFooter>
-            </Card>
-          </div>
-          <div className="mx-2" style={{ marginTop: '60px' }}>
+            <div className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <Card className='bg-slate-900'>
+                <img src={card} alt="Card 2 Header" className="object-cover w-full h-auto p-2" />
+                <CardBody>
+                  <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+                    <p className={`${styles.paragraph} ml-2`}>
+                      <span className='text-white'>Fastest</span> Copywriting Your {" "}
+                      <span className='text-white'>Website</span>
+                    </p>
+                  </div>
+                  <Typography variant="h5" color="blue-gray" className="mb-2 font-bold text-white">
+                    Lyft launching cross-platform
+                  </Typography>
+                  <p className={`${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at magna vitae lorem dapibus cursus. Nulla viverra diam sed mi ullamcorper aliquet.</p>
+                </CardBody>
+              </Card>
+            </div>
+
             {/* Card 3 */}
-            <Card className='bg-slate-900'>
-              <img src={card} alt="Card 3 Header" className="w-full h-auto p-2" />
-              <CardBody>
-                <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
-                  <p className={`${styles.paragraph} ml-2`}>
-                    <span className='text-white'>Fastest</span> Copywriting Your {" "}
-                    <span className='text-white'>Website</span>
-                  </p>
-                </div>
-                <Typography variant="h5" color="blue-gray" className="mb-2 font-bold text-white">
-                  Lyft launching cross-platform
-                </Typography>
-                <p className={`${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at magna vitae lorem dapibus cursus. Nulla viverra diam sed mi ullamcorper aliquet.</p>
-              </CardBody>
-              <CardFooter>
-                {/* Card 3 Footer */}
-              </CardFooter>
-            </Card>
+            <div className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <Card className='bg-slate-900'>
+                <img src={card} alt="Card 3 Header" className="object-cover w-full h-auto p-2" />
+                <CardBody>
+                  <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+                    <p className={`${styles.paragraph} ml-2`}>
+                      <span className='text-white'>Fastest</span> Copywriting Your {" "}
+                      <span className='text-white'>Website</span>
+                    </p>
+                  </div>
+                  <Typography variant="h5" color="blue-gray" className="mb-2 font-bold text-white">
+                    Lyft launching cross-platform
+                  </Typography>
+                  <p className={`${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at magna vitae lorem dapibus cursus. Nulla viverra diam sed mi ullamcorper aliquet.</p>
+                </CardBody>
+              </Card>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="flex justify-center mt-16">
+            {/* Card 1 */}
+            <div className="mx-2">
+              <Card className='bg-slate-900'>
+                <img src={card} alt="Card 1 Header" className="object-cover w-full h-auto p-2" />
+                <CardBody>
+                  <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+                    <p className={`${styles.paragraph} ml-2`}>
+                      <span className='text-white'>Fastest</span> Copywriting Your {" "}
+                      <span className='text-white'>Website</span>
+                    </p>
+                  </div>
+                  <Typography variant="h5" color="blue-gray" className="mb-2 font-bold text-white">
+                    Lyft launching cross-platform
+                  </Typography>
+                  <p className={`${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at magna vitae lorem dapibus cursus. Nulla viverra diam sed mi ullamcorper aliquet.</p>
+                </CardBody>
+              </Card>
+            </div>
+            {/* Card 2 */}
+            <div className="mx-2" style={{ marginTop: '30px' }}>
+              <Card className='bg-slate-900'>
+                <img src={card} alt="Card 2 Header" className="object-cover w-full h-auto p-2" />
+                <CardBody>
+                  <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+                    <p className={`${styles.paragraph} ml-2`}>
+                      <span className='text-white'>Fastest</span> Copywriting Your {" "}
+                      <span className='text-white'>Website</span>
+                    </p>
+                  </div>
+                  <Typography variant="h5" color="blue-gray" className="mb-2 font-bold text-white">
+                    Lyft launching cross-platform
+                  </Typography>
+                  <p className={`${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at magna vitae lorem dapibus cursus. Nulla viverra diam sed mi ullamcorper aliquet.</p>
+                </CardBody>
+              </Card>
+            </div>
+            {/* Card 3 */}
+            <div className="mx-2" style={{ marginTop: '60px' }}>
+              <Card className='bg-slate-900'>
+                <img src={card} alt="Card 3 Header" className="object-cover w-full h-auto p-2" />
+                <CardBody>
+                  <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+                    <p className={`${styles.paragraph} ml-2`}>
+                      <span className='text-white'>Fastest</span> Copywriting Your {" "}
+                      <span className='text-white'>Website</span>
+                    </p>
+                  </div>
+                  <Typography variant="h5" color="blue-gray" className="mb-2 font-bold text-white">
+                    Lyft launching cross-platform
+                  </Typography>
+                  <p className={`${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at magna vitae lorem dapibus cursus. Nulla viverra diam sed mi ullamcorper aliquet.</p>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        )}
       </div>
     </section>
-  )
+  );
 }
 
 export default Features;
