5153b760c3bec9ba84fe909fe39b8a346dfc924d
Akshay200228 <sankpla2812@gmail.com>
2023-06-25 20:46:28 +0530

    cleanup code

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,24 +2,13 @@ import airbnb from "./airbnb.png";
 import bill from "./bill.png";
 import binance from "./binance.png";
 import card from "./card.png";
-import card1 from "./card1.png";
-import card2 from "./card2.png";
-import card3 from "./card3.png";
 import logo from "./logo.svg";
-import logo2 from "./logo2.png";
 import quotes from "./quotes.svg";
-import robot from "./robot.png";
-import send from "./Send.svg";
-import shield from "./Shield.svg";
-import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import arrowUp from "./arrow-up.svg";
 import discount from "./Discount.svg";
 import wavo from "./wavo.png";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
 import winwap from "./winwap.png";
 import heineken from "./heineken.png";
 import berloni from "./berloni.png";
@@ -34,23 +23,12 @@ export {
   bill,
   binance,
   card,
-  card1,
-  card2,
-  card3,
   logo,
   quotes,
-  robot,
-  send,
-  shield,
-  star,
   menu,
   close,
   arrowUp,
   discount,
-  people01,
-  people02,
-  people03,
-  logo2,
   wavo,
   winwap,
   heineken,
